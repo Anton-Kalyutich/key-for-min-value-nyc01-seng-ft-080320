00259a5e5e58cd95abaa4c62e2f6937976068071
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-19 14:37:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  keys = []
   values = []
   name_hash.each do |key, value|
     values << value
