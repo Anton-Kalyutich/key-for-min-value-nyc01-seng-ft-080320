49c2cf67f25a8a50df2a6be567cae6a941ffc41e
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-19 14:43:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,5 +13,6 @@ def key_for_min_value(name_hash)
     if value < min_value
       min_value = value
     end
-  values.find_index(min_value)
+  index = values.find_index(min_value)
+  keys[index]
 end
